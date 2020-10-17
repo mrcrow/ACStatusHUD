d32c6f72cbabe1f18176b000cb474c250922caeb
WU Wenzhi <wwz.michael@gmail.com>
2020-10-17 13:08:24 +0800

    First init

--- Example/ACStatusHUD/ACViewController.m
+++ Example/ACStatusHUD/ACViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ACViewController.h"
+#import <ACStatusHUD/ACStatusHUD.h>
 
 @interface ACViewController ()
 
@@ -18,6 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [ACStatusHUD presentTitle:@"New card added" message:nil iconStyle:ACStatusIconTypeCard];
+    });
 }
 
 - (void)didReceiveMemoryWarning
